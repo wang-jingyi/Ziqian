394d46f101ed4eb95c621a6d820f81f9809f089e
Jingyi Wang <wangjyee@gmail.com>
2016-10-11 18:43:13 +0800

    active learning config file.

---
+++ src/main/java/io/github/wang_jingyi/ZiQian/active/ALConfig.java
@@ -0,0 +1,9 @@
+package io.github.wang_jingyi.ZiQian.active;
+
+public class ALConfig {
+
+	public static boolean sparse = false;   // if the matrix is sparse
+	public static int stateNumber = 0; 	// number of states
+	public static int newSampleNumber = 10; // number of new samples
+	public static int pathLength = 500;
+}
