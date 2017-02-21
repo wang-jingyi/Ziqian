31ac6b1625bf7858d09c1172ed14e1db8fb64631
wangjingyi <wangjyee@gmail.com>
2017-02-21 10:19:36 +0800

    add comments to examples.

--- src/main/java/io/github/wang_jingyi/ZiQian/example/CrowdPositive.java
+++ src/main/java/io/github/wang_jingyi/ZiQian/example/CrowdPositive.java
@@ -7,6 +7,11 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * property to verify for Crowds of PRISM benchmark: http://www.prismmodelchecker.org/casestudies/crowds.php
+ * a property should implement Predicate and Serializable (for saving objects) interface
+ * */
+
 public class CrowdPositive implements Predicate, Serializable{
 	
 	/**
@@ -16,25 +21,25 @@ public class CrowdPositive implements Predicate, Serializable{
 	List<String> vars = new ArrayList<>();
 	
 	public CrowdPositive() {
-		vars.add("observe0");
+		vars.add("observe0"); // add the relevant variables
 	}
 
 	@Override
-	public List<String> getVariables() {
+	public List<String> getVariables() { // return the relevant variables of the predicate
 		return vars;
 	}
 
 	@Override
-	public boolean check(VariablesValue vv) {
-		int observe_0 = vv.getVarValue(vars.get(0)).getRawIntValue();
-		if(observe_0>1){
+	public boolean check(VariablesValue vv) { // check if the variable valuation of a state satisfies the predicate (property)
+		int observe_0 = vv.getVarValue(vars.get(0)).getRawIntValue(); // get the value of observe_0 of current state
+		if(observe_0>1){ // check if the valuation of the variables in the current state satisfies the predicate
 			return true;
 		}
 		return false;
 	}
 
 	@Override
-	public String getPredicateName() {
+	public String getPredicateName() { // return the name of the predicate
 		return "positive";
 	}
 
