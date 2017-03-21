ae7f1da87427f1089beaf3b0aed78fb037f753f3
DESKTOP-P6CNSGI\jiayi <jiayifan21@gmail.com>
2017-03-21 16:56:48 +0800

    add back...

---
+++ src/main/java/io/github/wang_jingyi/ZiQian/run/PlatformDependent.java
@@ -0,0 +1,11 @@
+package io.github.wang_jingyi.ZiQian.run;
+
+public class PlatformDependent {
+	
+	public static final String USER_HOME_DIR = System.getProperty("user.home");
+	public static final String CAV_MODEL_ROOT = USER_HOME_DIR +"\\ziqian_evaluation_cav";
+	public static final String PRISM_PATH = "C:\\Program Files\\prism-4.3.1\\bin";
+	public static final String SWAT_SIMULATE_PATH = USER_HOME_DIR +"\\swat_beta_v2_1";
+	public static final String SWAT_EVALUATION_PATH = "C:\\Users\\jiayi\\Dropbox\\PhD\\Research\\DATA";
+	
+}
